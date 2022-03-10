5273408b5f463b4ac200568f762948e5264c6226
Alonso <alonso.manilla@gmai.com>
2022-03-10 17:40:46 -0600

    Google Ads

---
+++ components/GoogleAd.js
@@ -0,0 +1,23 @@
+import {useEffect} from 'react';
+
+export function GoogleAd() {
+  useEffect(() => {
+    try {
+      // @ts-ignore
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
+
+  return (
+    <ins
+      className="adsbygoogle"
+      style={{display: 'block'}}
+      data-ad-client={process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}
+      data-ad-slot="5733408918"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    />
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_GOOGLE_ADSENSE: 'ca-pub-5920437867428524',
+  }
 }
